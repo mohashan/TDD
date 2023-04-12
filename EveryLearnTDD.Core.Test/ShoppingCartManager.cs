455a2bc4a5154288d6af0c1c96c22b980e1b2fc9
mohashan <msh200x@gmail.com>
2023-04-12 15:14:01 +0330

    Add Single article and multiple same and varied Article are tested

--- EveryLearnTDD.Core.Test/ShoppingCartManager.cs
+++ EveryLearnTDD.Core.Test/ShoppingCartManager.cs
@@ -2,15 +2,26 @@
 {
     internal class ShoppingCartManager
     {
+        private List<AddToCartItem> _shoppingCart;
         public ShoppingCartManager()
         {
+            _shoppingCart = new List<AddToCartItem>();
         }
 
         internal AddToCartResponse AddToCart(AddToCartRequest request)
         {
+            var ExistItem = _shoppingCart.FirstOrDefault(x => x.ArticleId == request.item.ArticleId);
+            if (ExistItem == null)
+            {
+                _shoppingCart.Add(request.item);
+            }
+            else
+            {
+                ExistItem.Quantity += request.item.Quantity;
+            }
             return new AddToCartResponse
             {
-                Items = new AddToCartItem[] { request.item }
+                Items = _shoppingCart.ToArray()
             };
         }
     }
--- EveryLearnTDD.Core.Test/ShoppingCartTests.cs
+++ EveryLearnTDD.Core.Test/ShoppingCartTests.cs
@@ -35,5 +35,81 @@ namespace EveryLearnTDD.Core
             Assert.IsNotNull(response);
             Assert.Contains(item, response.Items);
         }
+
+        [Test]
+        public void ShouldReturnArticlesAddedToCart()
+        {
+            var item1 = new AddToCartItem()
+            {
+                ArticleId = 42,
+                Quantity = 5
+            };
+
+            var request = new AddToCartRequest()
+            {
+                item = item1
+            };
+
+            var manager = new ShoppingCartManager();
+
+            AddToCartResponse response = manager.AddToCart(request);
+
+            var item2 = new AddToCartItem()
+            {
+                ArticleId = 32,
+                Quantity = 2
+            };
+
+            request = new AddToCartRequest()
+            {
+                item = item2
+            };
+
+            response = manager.AddToCart(request);
+            response = manager.AddToCart(request);
+
+
+            Assert.IsNotNull(response);
+            Assert.Contains(item1, response.Items);
+            Assert.Contains(item2, response.Items);
+        }
+
+        [Test]
+        public void ShouldReturnCombinedQuantityWhenSameArticleAddedToCart()
+        {
+            var item1 = new AddToCartItem()
+            {
+                ArticleId = 42,
+                Quantity = 5
+            };
+
+            var request = new AddToCartRequest()
+            {
+                item = item1
+            };
+
+            var manager = new ShoppingCartManager();
+
+            AddToCartResponse response = manager.AddToCart(request);
+
+            var item2 = new AddToCartItem()
+            {
+                ArticleId = 42,
+                Quantity = 2
+            };
+
+            request = new AddToCartRequest()
+            {
+                item = item2
+            };
+
+            response = manager.AddToCart(request);
+
+
+            Assert.IsNotNull(response);
+            Assert.IsNotNull(response.Items);
+            Assert.That(response.Items.Length == 1);
+            Assert.That(response.Items.FirstOrDefault(c=>c.ArticleId == 42).Quantity == 7);
+        }
     }
 }
